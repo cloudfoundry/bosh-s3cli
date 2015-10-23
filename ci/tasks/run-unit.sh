b1cbba9b5058ba68b58da3aa63ce94c9d3ae7953
Caleb Miles <cmiles@pivotal.io>
2015-10-23 15:37:15 -0700

    Add unit tests to CI pipeline
    [#105489488](https://www.pivotaltracker.com/story/show/105489488)
    
    Signed-off-by: Edwin Xie <exie@pivotal.io>

---
+++ ci/tasks/run-unit.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+source s3cli/ci/tasks/utils.sh
+
+result=0
+
+pushd s3cli > /dev/null
+  echo -e "\n Vetting packages for potential issues..."
+  go vet s3cli/...
+  let "result+=$?"
+
+  echo -e "\n Checking with golint..."
+  golint s3cli/...
+  let "result+=$?"
+
+  echo -e "\n Testing packages..."
+  ginkgo -r -race src/s3cli
+  let "result+=$?"
+
+  echo -e "\n Running build script to confirm everything compiles..."
+  go build -o out/s3 s3cli/s3
+  let "result+=$?"
+
+  if [ $result -eq 0 ]; then
+  	echo -e "\nSUITE SUCCESS"
+  else
+  	echo -e "\nSUITE FAILURE"
+  fi
+popd > /dev/null
+
+exit $result
