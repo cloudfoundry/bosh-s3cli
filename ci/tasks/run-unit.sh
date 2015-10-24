c34633579d47e25f0b4ef6a3465cafd0616948e0
Edwin Xie <exie@pivotal.io>
2015-10-23 17:12:41 -0700

    Fail unit testing with set -e
    [#105489488](https://www.pivotaltracker.com/story/show/105489488):x
    
    Signed-off-by: Caleb Miles <cmiles@pivotal.io>
    Signed-off-by: Edwin Xie <exie@pivotal.io>

--- ci/tasks/run-unit.sh
+++ ci/tasks/run-unit.sh
@@ -4,32 +4,20 @@ set -e
 
 source s3cli/ci/tasks/utils.sh
 
-result=0
 
 pushd s3cli > /dev/null
   . .envrc
 
   echo -e "\n Vetting packages for potential issues..."
   go vet s3cli/...
-  let "result+=$?"
 
   echo -e "\n Checking with golint..."
   golint s3cli/...
-  let "result+=$?"
 
   echo -e "\n Testing packages..."
   ginkgo -r -race src/s3cli
-  let "result+=$?"
 
   echo -e "\n Running build script to confirm everything compiles..."
   go build -o out/s3 s3cli/s3
-  let "result+=$?"
-
-  if [ $result -eq 0 ]; then
-  	echo -e "\nSUITE SUCCESS"
-  else
-  	echo -e "\nSUITE FAILURE"
-  fi
 popd > /dev/null
 
-exit $result
