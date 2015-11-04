81c15dd128e29455246e3b2ac33346bd8037e343
caleb miles <cmiles@pivotal.io>
2015-11-03 16:14:26 -0800

    `go run` -> `go build`
    [#107225486](https://www.pivotaltracker.com/story/show/107225486)
    
    Signed-off-by: Tom Viehman <tjvman@gmail.com>

--- ci/tasks/run-unit.sh
+++ ci/tasks/run-unit.sh
@@ -27,7 +27,7 @@ pushd s3cli > /dev/null
   ginkgo -r -race src/s3cli
 
   echo -e "\n Running build script to confirm everything compiles..."
-  go run -ldflags "-X main.version ${version}" -o out/s3 s3cli/s3
+  go build -ldflags "-X main.version ${version}" -o out/s3 s3cli/s3
 
   echo -e "\n Testing version information"
   app_version=$(out/s3 -v)
