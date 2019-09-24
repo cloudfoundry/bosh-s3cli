bae3e8cb57ac0568c6e6ec4e10c49e93896e454b
Maya Rosecrance <mrosecrance@pivotal.io>
2019-09-23 17:07:02 -0700

    Revert "Move s3cli pipeline to director"
    - we have the develop -> master pattern and I commited this to master
    This reverts commit b641973cb6eede2de575683327dfd4ccc3e0dc33.

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-fly -t director sp -p s3cli -c ${PROJECT_DIR}/ci/pipeline.yml \
+fly -t cpi sp -p s3cli -c ${PROJECT_DIR}/ci/pipeline.yml \
   -l <(lpass show --notes "s3cli concourse secrets") \
   -l <(lpass show --notes "pivotal-tracker-resource-keys") \
   -l <(lpass show --note "bosh:docker-images concourse secrets")
