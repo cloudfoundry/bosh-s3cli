19f81e7d82366e9a12091f93ad1b753c3873b9c0
Rebecca Putinski <rputinski@pivotal.io>
2019-11-22 16:16:27 -0500

    Add lpass things to configure
    otherwise it explodes your terminal if you're not logged in

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+until lpass status;do
+  LPASS_DISABLE_PINENTRY=1 lpass ls a
+  sleep 1
+done
+
+until fly -t director status;do
+  fly -t director login
+  sleep 1
+done
+
 fly -t director sp -p s3cli -c ${PROJECT_DIR}/ci/pipeline.yml \
   -l <(lpass show --notes "s3cli concourse secrets") \
   -l <(lpass show --notes "pivotal-tracker-resource-keys") \
