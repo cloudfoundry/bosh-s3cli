525c8e67a137ded921cc4ed92817e46cbf99d621
Joseph Palermo <jpalermo@pivotal.io>
2021-08-03 13:28:13 -0700

    Move pipeline to bosh-ecosystem concourse

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
+project_dir="$( cd $(dirname $0)/.. && pwd )"
+
 until lpass status;do
   LPASS_DISABLE_PINENTRY=1 lpass ls a
   sleep 1
 done
 
-until fly -t director status;do
-  fly -t director login
-  sleep 1
-done
-
-fly -t director sp -p s3cli -c ${PROJECT_DIR}/ci/pipeline.yml \
+fly -t bosh-ecosystem sp -p bosh-s3cli -c ${project_dir}/ci/pipeline.yml \
   -l <(lpass show --notes "s3cli concourse secrets") \
   -l <(lpass show --notes "pivotal-tracker-resource-keys") \
   -l <(lpass show --note "bosh:docker-images concourse secrets")
