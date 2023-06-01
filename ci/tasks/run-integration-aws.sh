c49dd97091bfab70d2879aae87f50667398a4656
Long Nguyen <nguyenlo@vmware.com>
2023-06-01 08:17:10 -0400

    Updating to use role arn

--- ci/tasks/run-integration-aws.sh
+++ ci/tasks/run-integration-aws.sh
@@ -21,7 +21,20 @@ export PATH=${GOPATH}/bin:${PATH}
 export AWS_ACCESS_KEY_ID=${access_key_id}
 export AWS_SECRET_ACCESS_KEY=${secret_access_key}
 export AWS_DEFAULT_REGION=${region_name}
+export AWS_ROLE_ARN=${role_arn}
 stack_info=$(get_stack_info "${stack_name}")
+
+if [ -n "${AWS_ROLE_ARN}" ]; then
+  aws configure --profile creds_account set aws_access_key_id "${AWS_ACCESS_KEY_ID}"
+  aws configure --profile creds_account set aws_secret_access_key "${AWS_SECRET_ACCESS_KEY}"
+  aws configure --profile resource_account set source_profile "creds_account"
+  aws configure --profile resource_account set role_arn "${AWS_ROLE_ARN}"
+  aws configure --profile resource_account set region "${AWS_DEFAULT_REGION}"
+  unset AWS_ACCESS_KEY_ID
+  unset AWS_SECRET_ACCESS_KEY
+  unset AWS_DEFAULT_REGION
+  export AWS_PROFILE=resource_account
+fi
 
 # Some of these are optional
 export ACCESS_KEY_ID=${access_key_id}
